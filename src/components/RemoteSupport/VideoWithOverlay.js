b33bc118975b7eedad905e860ab6cf6247c25379
Lynden555 <diegollera1@gmail.com>
2025-10-14 11:49:51 -0700

    Actualizacion Licencias 11

--- src/components/RemoteSupport/VideoWithOverlay.js
+++ src/components/RemoteSupport/VideoWithOverlay.js
@@ -59,7 +59,6 @@ const VideoWithOverlay = ({
           border: "2px solid #143a66",
           display: status === "connected" ? "block" : "none",
           backgroundColor: "#000",
-          cursor: 'none',
           cursor: controlEnabled ? 'default' : 'default', // ✅ CRÍTICO: CURSOR NORMAL
           objectFit: 'contain'
         }}
